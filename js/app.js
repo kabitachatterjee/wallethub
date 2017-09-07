c348665ae56169eb02bc2b505747e2e07dcb6962
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-09-07 13:29:35 -0700

    changed to if-else from switch in phone number formatting

--- js/app.js
+++ js/app.js
@@ -37,27 +37,10 @@ app.controller('Ctrl', ['$scope', function($scope) {
 
         var country, city, number;
 
-        switch (value.length) {
-            case 10: // +1PPP####### -> C (PPP) ###-####
-                country = 1;
-                city = value.slice(0, 3);
-                number = value.slice(3);
-                break;
-
-            case 11: // +CPPP####### -> CCC (PP) ###-####
-                country = value[0];
-                city = value.slice(1, 4);
-                number = value.slice(4);
-                break;
-
-            case 12: // +CCCPP####### -> CCC (PP) ###-####
-                country = value.slice(0, 3);
-                city = value.slice(3, 5);
-                number = value.slice(5);
-                break;
-
-            default:
-                return tel;
+        if (value.length <= 10) {
+          country = 1;
+          city = value.slice(0, 3);
+          number = value.slice(3);
         }
 
         if (country == 1) {
